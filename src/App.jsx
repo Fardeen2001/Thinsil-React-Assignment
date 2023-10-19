4df60a74d561e51de7027bdbe58c4f01379a3512
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-10-19 21:33:31 +0530

    navbar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from "./components/header/Navbar";
+
 function App() {
   return (
     <>
-      {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
-      {/*checking tailwind is working or not */}
+      <Navbar />
     </>
   );
 }
